3fc6f3e4b8214036b1e00c15b8d277197b027880
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-04-26 05:33:23 +0800

    ref: added pathing for subpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import ServicesPage from './pages/services/ServicePage';
+import ServicesPage from './pages/services/ServicesPage';
+import ServicesInfoPage from './pages/services/ServiceInfoPage';
 import MapPage from './pages/map/MapPage';
 import AccountPage from './pages/account/AccountPage';
 
@@ -7,14 +8,23 @@ export default function App() {
     return (
         <BrowserRouter>
             <nav>
-                <Link to="/">Services</Link>
+                <Link to="/services">Services</Link>
                 <Link to="/map">Map</Link>
                 <Link to="/account">Account</Link>
             </nav>
  
             <Routes>
+                {/* default routing (routes to services page) */}
                 <Route path='/' element={<ServicesPage/>}/>
+
+                {/* service page routing*/}
+                <Route path='/services' element={<ServicesPage/>}/>
+                <Route path='/services/info' element={<ServicesInfoPage/>}/>
+
+                {/* map page routing*/}
                 <Route path='/map' element={<MapPage/>}/>
+                
+                {/* account page routing*/}
                 <Route path='/account' element={<AccountPage/>}/>
             </Routes>
         </BrowserRouter>
---
+++ src/pages/services/ServiceInfoPage.jsx
@@ -0,0 +1,3 @@
+export default function ServicesInfoPage() {
+    return <h1>Services Info Page</h1>
+}
