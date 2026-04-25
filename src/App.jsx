893eb3b7da31511c8fc8eee7c0133ad97f7b0e45
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-04-26 05:22:31 +0800

    feat: added routing for the three main pages (Services, Map, and Account) of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import ServicesPage from './pages/services/ServicePage';
+import MapPage from './pages/map/MapPage';
+import AccountPage from './pages/account/AccountPage';
 
-function App() {
+export default function App() {
     return (
-        <></>
+        <BrowserRouter>
+            <nav>
+                <Link to="/">Services</Link>
+                <Link to="/map">Map</Link>
+                <Link to="/account">Account</Link>
+            </nav>
+ 
+            <Routes>
+                <Route path='/' element={<ServicesPage/>}/>
+                <Route path='/map' element={<MapPage/>}/>
+                <Route path='/account' element={<AccountPage/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
+
