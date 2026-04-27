a35bfe5b55db37d57cd25f905245b3d4f8ca79b2
choco-melk <154525691+choco-melk@users.noreply.github.com>
2026-04-27 12:48:36 +0800

    ref: updated input field component

---
+++ src/components/form/InputField/InputField.jsx
@@ -0,0 +1,11 @@
+import './InputField.css';
+import { Icon } from './../../ui/';
+
+export function InputField({ icon="", placeholder="", className="" }) {
+    return (
+        <div className={`input-field ${className}`}>
+            {icon && <Icon name={icon} size="medium" className="input-icon"/>}
+            <input type='text' placeholder={placeholder} className={className}/>
+        </div>
+    );
+}
