fa0146216ec683c9cdbe93b4d6fcbbe22178fcb5
Neyro Caraig <neyrocaraig@gmail.com>
2026-05-05 21:33:40 +0800

    chore: updated config and package files

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Vite assumes 'public' is the root for static assets, 
-  // but you can change it if your CRA project used a different folder
-  publicDir: 'public', 
-  
-  // Optional: Set the base path if your app isn't deployed to the root domain
-  // base: '/my-app/', 
-})
+  publicDir: 'public',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
