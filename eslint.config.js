6dd2781d3bf1a17c68f5e587007a70dba5cfdb50
Neyro Caraig <neyrocaraig@gmail.com>
2026-05-05 21:33:07 +0800

    chore: added eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+const browserGlobals = {
+  AbortController: 'readonly',
+  alert: 'readonly',
+  console: 'readonly',
+  crypto: 'readonly',
+  document: 'readonly',
+  fetch: 'readonly',
+  indexedDB: 'readonly',
+  localStorage: 'readonly',
+  navigator: 'readonly',
+  requestAnimationFrame: 'readonly',
+  window: 'readonly',
+};
+
+const nodeGlobals = {
+  console: 'readonly',
+  process: 'readonly',
+};
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    ...js.configs.recommended,
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: browserGlobals,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-hooks/immutability': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    ...js.configs.recommended,
+    files: ['server/**/*.js', 'vite.config.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: nodeGlobals,
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+];
